0728e860f93cb24d0904e7a33d09ee4ee9570a54
Prajwal2204 <navadaprajwal@gmail.com>
2025-03-28 23:04:12 +0530

    chore: -

--- test.py
+++ test.py
@@ -6,9 +6,15 @@ print("Hello world")
 def areaOfRectangle(length, breadth):
     return length * breadth
 
+def areaOfTriangle(base, height):
+    return 0.5 * base * height
+
 def main():
     print("Inside main function")
     print("Area of rectangle is:", areaOfRectangle(a,b))
+    base = 4
+    height = 5
+    print("Area of triangle:", areaOfTriangle(base, height))
 
 if __name__ == "__main__":
     main()
